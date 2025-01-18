3aa8e00b690fa811da9470b33c1df0373a30269d
Ben Eichhoefer <ben.eichhofer@hotmail.com>
2025-01-18 12:16:37 +0000

    add code

---
+++ src/deck.py
@@ -0,0 +1,11 @@
+class Deck:
+    def __init__(self):
+        self.cards = []
+
+    def shuffle_deck(self):
+        # populate self.cards with 52 cards of deck
+        pass
+
+    def hit(self):
+        # pop from self.cards
+        pass
