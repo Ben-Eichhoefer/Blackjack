150842effaaea99a4f02fb1a323b63cbcf25c0e8
Ben Eichhoefer <ben.eichhofer@hotmail.com>
2025-01-19 16:29:05 +0000

    make card printable

--- src/card.py
+++ src/card.py
@@ -1,6 +1,13 @@
+from src.suit import Suit
 class Card:
+    suits = {Suit.CLUBS:"\u2663",Suit.DIAMONDS:"\u2666",Suit.HEARTS:"\u2665",Suit.SPADE:"\u2660"}
     def __init__(self,suit,value,type):
         self.suit = suit
         self.value = value
         self.type = type
+
+
+    def __str__(self):
+        return "%s %s" %  (self.type, self.suits[self.suit])
+    
 
